bdef5b080045e9ef9c878f17221c07ea18ed8fdc
unknown <waldoch.don@gmail.com>
2022-04-11 21:09:31 -0500

    rr-4-fetch-data-with-useeffect

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,18 +2,17 @@ import GalleryItem from './GalleryItem'
 
 function Gallery(props) {
 
-    const display = props.data.map((item, index) => {
-        return (
-            <GalleryItem item={item} key={index} />
-        )
-    })
-
+  const display = props.data.map((item, index) => {
     return (
-        <div>
-        {display}
-        </div>
+      <GalleryItem item={item} key={index} />
     )
-}
+  })
 
+  return (
+    <div>
+      {display}
+    </div>
+  )
+}
 
 export default Gallery
